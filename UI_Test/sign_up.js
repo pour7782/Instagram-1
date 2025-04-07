b4e88b3d83460910ea31b3643adbec6ea8bde7b5
Sanugie99 <Sanugie99@gmail.com>
2025-04-07 09:44:21 +0900

    2025.04.07

---
+++ UI_Test/sign_up.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    let logo = document.querySelector("#logo");
+    let id_input = document.querySelector("#id_input");
+    let xx = document.querySelector("#xx");
+    let idfal = document.querySelector("#idfal");
+    let pw_input = document.querySelector("pw_input");
+    let name_input = document.querySelector("name_input");
+    let email_input = document.querySelector("email_input");
+
+    id_input.addEventListener("submit", (e) => {
+        if([\w]{4,20} == false){
+
+        }
+    })
+
+    pw_input.addEventListener("submit", (e) => {
+
+    })
+
+    logo.addEventListener("click", (e) => {
+        
+    })
+
+})
